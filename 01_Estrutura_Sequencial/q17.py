2d2bed6b9c173320f19eff3d042648e0abaa63d6
Gabriel Q. Nunes <gabrielq.nunes@luizalabs.com>
2022-09-01 14:29:22 -0300

    Question 17 refactored

--- 01_Estrutura_Sequencial/q17.py
+++ 01_Estrutura_Sequencial/q17.py
@@ -3,20 +3,35 @@ import math
 class Main:
     def Run(self):
         area = float(input('Insert the area to be painted in square meters: '))
+        
+        self.CalculateGallonsQuantity(area)
+        
+        self.CalculateCansQuantity(area)
+        
+        self.CalculateGallonsAndCansQuantity(area)
+        
+    @staticmethod
+    def CalculateGallonsQuantity(area):
         paintRequired = area / 6
         paintCanQuantity = math.ceil(paintRequired / 18)
         totalPrice = paintCanQuantity * 80
 
         print('It will be required {} can(s) of paint.'.format(paintCanQuantity))
         print('Total price: AU$ {}'.format(totalPrice))
-
+        
+    @staticmethod
+    def CalculateCansQuantity(area):
+        paintRequired = area / 6
         paintCanQuantity = math.ceil(paintRequired / 3.6)
         totalPrice = paintCanQuantity * 25
 
         print('It will be required {} gallons(s) of paint.'.format(paintCanQuantity))
         print('Total price: AU$ {}'.format(totalPrice))
 
-        paintCanQuantity = math.floor(paintRequired / 18)
+    @staticmethod
+    def CalculateGallonsAndCansQuantity(area):
+        paintRequired = area / 6
+        paintCanQuantity = paintRequired // 18
         totalCanPrice = paintCanQuantity * 80
 
         paintRest = paintRequired % 18
@@ -27,9 +42,8 @@ class Main:
         totalPrice = totalGallonPrice + totalCanPrice
 
         print('It will be required {} can(s) of paint and {} gallon(s) of paint.'
-             .format(paintCanQuantity, paintGallonQuantity))
+            .format(paintCanQuantity, paintGallonQuantity))
         print('Total price: AU$ {}'.format(totalPrice))
-
 
 main = Main()
 main.Run()
